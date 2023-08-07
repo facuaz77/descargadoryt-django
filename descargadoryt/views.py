bb3761bfa966673dbf4356fa9ae478e104c46831
facuaz77 <alanizfacundo902@gmail.com>
2023-08-07 19:29:42 -0300

    new update

--- descargadoryt/views.py
+++ descargadoryt/views.py
@@ -1,43 +1,53 @@
-import os
-from django.http import FileResponse, render, StreamingHttpResponse
+from django.shortcuts import render,HttpResponse
+import requests
 from pytube import YouTube
 from pytube.exceptions import PytubeError
+
 
 def home(request):
     mensaje = ""
     if request.method == 'POST':
         link = request.POST.get('link', '')
         formato = request.POST.get('formato', 'mp4')
-        calidad_video = request.POST.get('calidad_video', 'highest')
+        calidad_video = request.POST.get('calidad_video', 'highest') 
         calidad_audio = request.POST.get('calidad_audio', 'highest')
 
         try:
-            # Descargar el video y devolver la respuesta con el archivo descargado
-            response = descargar_video(request, link, formato, calidad_video, calidad_audio)
-            return response
+            # Descargar el contenido del video/audio
+            archivo, extension = descargar_video_audio(link, formato, calidad_video, calidad_audio)
 
+            # Crear una respuesta HTTP para la descarga
+            response = HttpResponse(archivo, content_type=f"video/{extension}")
+            response['Content-Disposition'] = f'attachment; filename="video.{extension}"'
+            mensaje = f"Descarga completada!"
+            return response
         except PytubeError as e:
             mensaje = f"Error en la descarga: {e}"
 
     return render(request, 'home.html', {'mensaje': mensaje})
 
-
-def video_stream(url, chunk_size=8192):
-    yt = YouTube(url)
-    stream = yt.streams.filter(progressive=True, file_extension='mp4').first()
-
-    if stream:
-        return stream.url
-    else:
-        raise PytubeError("No se encontró una fuente de video progresiva.")
-
-
-def descargar_video(request, url, formato='mp4', calidad_video='highest', calidad_audio='highest'):
+def descargar_video_audio(url, formato='mp4', calidad_video='highest', calidad_audio='highest' ):
     try:
-        video_url = video_stream(url)
-        response = StreamingHttpResponse(video_stream(url), content_type='video/mp4')
-        response['Content-Disposition'] = f'attachment; filename="video.mp4"'
-        return response
+        # Crea una instancia de la clase YouTube
+        yt = YouTube(url)
 
-    except PytubeError as e:
-        raise PytubeError(f"Error en la descarga: {e}")
+        # Obtiene el flujo de video o audio según el formato seleccionado
+        if formato == 'mp4':
+            stream = yt.streams.filter(file_extension='mp4', res=calidad_video).first()
+        elif formato == 'mp3':
+            stream = yt.streams.filter(only_audio=True, abr=calidad_audio).first()
+        else:
+            raise ValueError("Formato no válido. Debe ser 'mp4' o 'mp3'.")
+
+        if stream:
+          
+            response = requests.get(stream.url)
+            archivo = response.content
+            extension = formato
+
+            return archivo, extension
+        else:
+            raise PytubeError("No se encontró la calidad especificada.")
+    except Exception as e:
+        raise PytubeError(f"Ocurrió un error: {e}")
+
