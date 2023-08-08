5bfb7eca979441c8aa99cbb5978586c588bb4c06
facuaz77 <alanizfacundo902@gmail.com>
2023-08-07 21:58:38 -0300

    .

--- descargadorproyecto/settings.py
+++ descargadorproyecto/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 import os
 import dj_database_url
 
+
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
