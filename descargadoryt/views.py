a791571a786480f2a2aa2007bdfc9c482fa6c9e1
facuaz77 <alanizfacundo902@gmail.com>
2023-08-07 22:21:46 -0300

    .

--- descargadoryt/views.py
+++ descargadoryt/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, HttpResponse
+import io, logging, requests
+from django.http import StreamingHttpResponse
+from django.shortcuts import render
 from pytube import YouTube, Stream
 from pytube.exceptions import PytubeError
-import requests
-import logging
 
 def home(request):
     mensaje = ""
@@ -17,7 +17,7 @@ def home(request):
             archivo, extension = descargar_video_audio(link, formato, calidad_video, calidad_audio)
 
             # Crear una respuesta HTTP para la descarga
-            response = HttpResponse(archivo, content_type=f"video/{extension}")
+            response = StreamingHttpResponse(archivo, content_type=f"video/{extension}")
             response['Content-Disposition'] = f'attachment; filename="video.{extension}"'
             mensaje = f"Descarga completada!"
             return response
@@ -40,16 +40,21 @@ def descargar_video_audio(url, formato='mp4', calidad_video='highest', calidad_a
             raise ValueError("Formato no válido. Debe ser 'mp4' o 'mp3'.")
 
         if stream:
-            response = requests.get(stream.url)
-            archivo = response.content
-            extension = stream.subtype
+            response = get_stream_response(stream)
+            archivo = response.iter_content(chunk_size=4096)
 
-            return archivo, extension
+            return archivo, stream.subtype
         else:
             raise PytubeError("No se encontró la calidad especificada.")
     except Exception as e:
         logging.error(f"Error durante la descarga del video: {e}")
         raise PytubeError(f"Ocurrió un error durante la descarga del video: {e}")
+
+def get_stream_response(stream: Stream):
+    # Obtener el contenido del flujo de video o audio
+    response = requests.get(stream.url, stream=True)
+    return response
+
 
 def get_best_video_stream(streams: Stream, calidad: str) -> Stream:
     # Filtra los flujos de video disponibles por calidad
